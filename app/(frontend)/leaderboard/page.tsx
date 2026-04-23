341dad1b689ca0eb6972e66e9c9c8eed36f0a4b5
Tom Suh <Tomsuh214@gmail.com>
2026-04-23 14:34:54 +1200

    changed all a:tags to Link:tags for next.js compatibility

--- app/(frontend)/leaderboard/page.tsx
+++ app/(frontend)/leaderboard/page.tsx
@@ -1,6 +1,7 @@
 import { getPayload } from 'payload'
 import config from '@payload-config'
 import PlayerRowDisplay from './playerRowDisplay' 
+import Link from 'next/link'
 
 export default async function LeaderboardPage() { //api part
   const payload = await getPayload({ config })
@@ -13,7 +14,7 @@ export default async function LeaderboardPage() { //api part
   return ( //no styles.css file, can delete and change this whenever, just a placeholder
     <div className="p-8 font-sans flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-6">Top 10 Leaderboard</h1>
-      <a href="/" className="mb-4 text-blue-500 hover:underline">Go back to Dashboard</a>
+      <Link href="/" className="mb-4 text-blue-500 hover:underline">Go back to Dashboard</Link>
 
       <div className="flex flex-col w-full items-center">
         {topPlayers.docs.map((player, index) => (
