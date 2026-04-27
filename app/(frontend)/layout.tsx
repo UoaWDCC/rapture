f73d9bd77c2662512513b4aaea69d0b449aadba0
Zebony919 <zebony9@gmail.com>
2026-04-27 16:38:27 +1200

    Feat/footer issue9 (#13)
    * feat:footer-component
    
    Created footer component with optional external links and text props.
    
    * feat:footer-component
    
    Add Nav links and props to the footer
    
    * feat:footer-component
    
    Add optional text and title. Updates Structure/Layout
    
    ---------
    
    Co-authored-by: Shazeel Ali <75409190+ShazeelAli@users.noreply.github.com>

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./styles.css";
+import Footer from "./components/Footer";
 
 export const metadata = {
   description: "Website For Studio Rapture",
   title: "Studio Rapture",
 };
+
+// Fill in with actual webpage links when they are done.
+const navigationLinks = [
+  {label: "Home", href: "/"},
+  {label: "About", href: "/"}, 
+  {label: "Our Games", href: "/"}
+]
+
+// Add whichever socials Rapture wants here.
+const externalLinks = [
+  { label: "Twitter", href: "https://twitter.com" },
+  { label: "Discord", href: "https://discord.com" },
+  { label: "Instagram", href: "https://instagram.com"}
+];
+
+// Add any additional text.
+const title = "Rapture Games";
+const text1 = "Want to be notified of our upcoming games!";
+const text2 = "Make sure to follow us on our socials.";
+const copyright = "© 2026 Studio Rapture. All rights reserved.";
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props;
@@ -12,6 +33,14 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
     <html lang="en">
       <body>
         <main>{children}</main>
+        <Footer 
+          navigationLinks={navigationLinks}
+          externalLinks={externalLinks}
+          title={title}
+          text1={text1}
+          text2={text2}
+          copyright={copyright}
+        />
       </body>
     </html>
   );
