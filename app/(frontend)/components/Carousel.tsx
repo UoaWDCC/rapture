2f568307e31a5f3f46ae4d644c5e8a2bc03930a9
|rene <tendean.ireneandyna@gmail.com>
2026-04-30 13:38:04 +1200

    Changed Cursor + Adds Dots

--- app/(frontend)/components/Carousel.tsx
+++ app/(frontend)/components/Carousel.tsx
@@ -36,7 +36,7 @@ export default function Carousel({items, autoSlide = true, autoSlideInterval = 3
     };
 
   return (
-    <div className="relative w-100000 max-w-3xl mx-auto">
+    <div className="relative w-100000 h-full max-w-3xl mx-auto">
         <div className="overflow-hidden relative h-64">
             {items.map((item, index) => (
                 <div
@@ -51,9 +51,19 @@ export default function Carousel({items, autoSlide = true, autoSlideInterval = 3
             ))}
         </div>
 
-        {/* Controls */}
-        <button className="absolute top-1/2 left-0 transform -translate-y-1/2" onClick={prevSlide}>Prev</button>
-        <button className="absolute top-1/2 right-0 transform -translate-y-1/2" onClick={nextSlide}>Next</button>
+        {/* Controls - simple buttons */}
+        <button className="absolute top-1/2 left-0 transform -translate-y-1/2 cursor-pointer" onClick={prevSlide}>Prev</button>
+        <button className="absolute top-1/2 right-0 transform -translate-y-1/2 cursor-pointer" onClick={nextSlide}>Next</button>
+
+        <div className="absolute bottom-0 left-0 right-0 flex justify-center mb-4">
+        {items.map((_, index) => (
+          <div
+            key={index}
+            className={`w-2 h-2 rounded-full mx-1 ${index === currentIndex ? 'bg-gray-800' : 'bg-gray-400'}`}
+            onClick={() => setCurrentIndex(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
