4fb0dc881f07fcab602693c3c3827343984a719b
iTzKingJames <jamesa2362@gmail.com>
2026-05-07 01:49:46 +1200

    feat: add image rendering to product display

--- app/(frontend)/products/components/productsDisplay.tsx
+++ app/(frontend)/products/components/productsDisplay.tsx
@@ -9,7 +9,11 @@ export default async function ProductsDisplay({
 
   return (
     <div className="m-20">
+      {product.image && typeof product.image !== "string" ? (
+        <img src={product.image.url ?? ""} alt={product.image.alt ?? product.name} className="w-80 h-80 object-cover mb-3" />
+      ) : (
       <div className="w-80 h-80 bg-gray-100 mb-3"></div>
+      )}
       <p className="font-bold text-xl">{product.name}</p>
       <p className="text-lg">{formattedPrice} {product.currency}</p>
       <p>{product.description}</p>
