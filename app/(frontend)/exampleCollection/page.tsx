a274501504ab3ec735753872146dcf7601fbc651
Tom Suh <Tomsuh214@gmail.com>
2026-05-07 13:29:56 +1200

    Added button functionality

--- app/(frontend)/exampleCollection/page.tsx
+++ app/(frontend)/exampleCollection/page.tsx
@@ -3,7 +3,7 @@ import config from "@/payload.config";
 import ExampleCollectionDisplay from "./components/exampleCollectionDisplay";
 
 
-export default async function ExampleColectionPage() {
+export default async function ExampleCollectionPage() {
   const payload = await getPayload({ config: await config });
 
   const examples = await payload.find({
