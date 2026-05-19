cb0525058601d82e39d0f6a8f864c7625b0dbc99
iTzKingJames <jamesa2362@gmail.com>
2026-05-20 00:06:38 +1200

    feat: adjust disc component sizing and social media section layout

--- app/(frontend)/components/Disc.tsx
+++ app/(frontend)/components/Disc.tsx
@@ -16,8 +16,8 @@ interface DiscProps {
 export default function Disc({ name, url, image, icon, iconWidth, iconHeight, iconRight, iconTop, color }: DiscProps) {
     return (
         <Link href={url} target="_blank">
-            <div className={`relative w-44 h-52 ${color} rounded-xl overflow-hidden`}>
-                <Image src={image} width={192} height={224} alt={name} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-48" />
+            <div className={`relative w-65 h-75 ${color} rounded-xl overflow-hidden`}>
+                <Image src={image} width={192} height={224} alt={name} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full" />
                 {icon && <Image src={icon} alt={name + " icon"} width={iconWidth || 56} height={iconHeight || 56} 
                 className= {`absolute ${iconTop || 'top-4'} ${iconRight || 'right-2'} object-contain`} />}
                 <p className="absolute bottom-5 left-1/2 -translate-x-1/2 text-white text-lg uppercase" 
--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -32,7 +32,7 @@ export default async function HomePage() {
   });
 
   return (
-    <div className="w-full flex flex-col items-center">
+    <div className="w-full flex-col items-center">
       <div className="w-full min-h-screen flex flex-col flex-wrap items-center justify-center gap-5">
         <picture>
           <source srcSet="/Rapture_Large_2500-1000.png" />
@@ -59,17 +59,21 @@ export default async function HomePage() {
 
         <div className="flex items-center gap-4 w-full px-8 mb-4">
     <div className="flex-1 border-t border-white"></div>
-    <h2 className="text-white text-2xl" style={{ fontFamily: "'Nova Cut', cursive" }}>SOCIAL MEDIA</h2>
+    <p className="text-white text-lg" style={{ fontFamily: "'Nova Cut', cursive" }}>SOCIAL MEDIA</p>
     <div className="flex-1 border-t border-white"></div>
     </div>
 
-        <div className="flex gap-4 justify-center w-full">
+        <div className="flex gap-8 justify-center w-full px-20">
     <Disc name="Discord" url="https://discord.com" image="/dvd_player_2.png" icon="/DISCORD.png" 
-    iconWidth={40} iconHeight={40} iconTop="top-6.5" iconRight="right-4" color="bg-[#171947]" />
+    iconWidth={45} iconHeight={45} iconTop="top-10" iconRight="right-7.5" color="bg-[#171947]" />
     <Disc name="Youtube" url="https://www.youtube.com/@STUDIO_RAPTURE" image="/dvd_player_2.png" icon="/YT.png" 
-    iconWidth={70} iconHeight={70} iconTop="top-3" iconRight="right-0.25" color="bg-[#4C1010]" />
+    iconWidth={80} iconHeight={80} iconTop="top-6.5" iconRight="right-3" color="bg-[#4C1010]" />
     <Disc name="Steam" url="https://store.steampowered.com/search/?developer=Lee%20Wilson" image="/dvd_player_2.png" icon="/steam.png" 
-    iconWidth={36} iconHeight={36} iconTop="top-7" iconRight="right-4.5" color="bg-[#171720]" />
+    iconWidth={45} iconHeight={45} iconTop="top-10" iconRight="right-7" color="bg-[#171720]" />
+    </div>
+
+    <div className="w-full px-8 mt-8">
+      <div className="border-t border-white"></div>
     </div>
 
         <div className="mt-10">
