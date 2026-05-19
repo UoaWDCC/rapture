2f270febd1fbb6038d06505d2937b655f57b8996
iTzKingJames <jamesa2362@gmail.com>
2026-05-20 01:50:05 +1200

    feat: add background designs to hero and social media sections

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -33,8 +33,29 @@ export default async function HomePage() {
 
   return (
     <div className="w-full flex-col items-center">
-      <div className="w-full min-h-screen flex flex-col flex-wrap items-center justify-center gap-5">
-        <picture>
+      <div className="w-full min-h-screen flex flex-col flex-wrap items-center justify-center gap-5 relative overflow-hidden">
+       
+        <div className="absolute top-0 left-0 w-full">
+        <Image
+          src="/images/IMG_3161 1.png"
+          alt=""
+          width={1437}
+          height={500}
+          className="w-full object-cover opacity-60"
+        />
+      </div>
+
+        <div className="absolute bottom-0 left-0 w-full">
+        <Image
+          src="/images/FRONT PAGE.png"
+          alt=""
+          width={1437}
+          height={500}
+          className="w-full object-cover opacity-60"
+        />
+      </div>
+       
+       <picture>
           <source srcSet="/Rapture_Large_2500-1000.png" />
           <Image
             className="mt-20"
@@ -56,20 +77,33 @@ export default async function HomePage() {
         {/* PROMOTED GAME SECTION - to be added when component is ready */}
 
         <NewsSection />
+       
+      <div className="relative w-full overflow-hidden">
+  
+  <div className="absolute bottom-0 left-0 w-full h-[300px] overflow-hidden">
+    <Image
+      src="/images/FOOTER.png"
+      alt=""
+      width={1437}
+      height={400}
+      className="w-full object-cover opacity-60"
+    />
+  </div>
 
-        <div className="flex items-center gap-4 w-full px-8 mb-4">
-    <div className="flex-1 border-t border-white"></div>
-    <p className="text-white text-lg" style={{ fontFamily: "'Nova Cut', cursive" }}>SOCIAL MEDIA</p>
-    <div className="flex-1 border-t border-white"></div>
+  <div className="relative z-10">
+    <div className="flex items-center gap-4 w-full px-8 mb-4">
+      <div className="flex-1 border-t border-white"></div>
+      <p className="text-white text-lg" style={{ fontFamily: "'Nova Cut', cursive" }}>SOCIAL MEDIA</p>
+      <div className="flex-1 border-t border-white"></div>
     </div>
 
-        <div className="flex gap-8 justify-center w-full flex-wrap">
-    <Disc name="Discord" url="https://discord.com" image="/dvd_player_2.png" icon="/DISCORD.png" 
-    iconWidth={45} iconHeight={45} iconTop="top-10" iconRight="right-7.5" color="bg-[#171947]" />
-    <Disc name="Youtube" url="https://www.youtube.com/@STUDIO_RAPTURE" image="/dvd_player_2.png" icon="/YT.png" 
-    iconWidth={80} iconHeight={80} iconTop="top-6.5" iconRight="right-3" color="bg-[#4C1010]" />
-    <Disc name="Steam" url="https://store.steampowered.com/search/?developer=Lee%20Wilson" image="/dvd_player_2.png" icon="/steam.png" 
-    iconWidth={45} iconHeight={45} iconTop="top-10" iconRight="right-7" color="bg-[#171720]" />
+    <div className="flex gap-8 justify-center w-full flex-wrap">
+      <Disc name="Discord" url="https://discord.com" image="/dvd_player_2.png" icon="/DISCORD.png" 
+        iconWidth={45} iconHeight={45} iconTop="top-10" iconRight="right-7.5" color="bg-[#171947]" />
+      <Disc name="Youtube" url="https://www.youtube.com/@STUDIO_RAPTURE" image="/dvd_player_2.png" icon="/YT.png" 
+        iconWidth={80} iconHeight={80} iconTop="top-6.5" iconRight="right-3" color="bg-[#4C1010]" />
+      <Disc name="Steam" url="https://store.steampowered.com/search/?developer=Lee%20Wilson" image="/dvd_player_2.png" icon="/steam.png" 
+        iconWidth={45} iconHeight={45} iconTop="top-10" iconRight="right-7" color="bg-[#171720]" />
     </div>
 
     <div className="w-full px-8 mt-8">
@@ -81,6 +115,8 @@ export default async function HomePage() {
         <p className="text-white text-lg">TEXT WILL GO HERE</p>
       </div>
     </div>
+  </div>
+</div>
 
         <div className="mt-10 text-center w-full px-8">
           <h1 className="text-2xl font-bold">
