ad61156cfa60e41e15ccdf495f9ed3314145a376
|rene <tendean.ireneandyna@gmail.com>
2026-05-26 21:36:10 +1200

    Styling Added
    Styling added and tested

--- app/(frontend)/components/ui/GamePopUp.tsx
+++ app/(frontend)/components/ui/GamePopUp.tsx
@@ -19,12 +19,12 @@ type popUpProps = textPopUp | imgPopUp
 export default function PopUp(prop: popUpProps) { {/*title is reqiured ... or is it?*/}
     if ("title" in prop) {
         return(
-            <div style={{ width: `${prop.width}px`, height: `${prop.height}px` }} className="max-h-1000 inline-block border-2 border-white m-[2.5%]">
+            <div style={{ width: `${prop.width}px`, height: `${prop.height}px` }} className="inline-block m-[2.5%] border-l border-r border-b border-dotted border-[#D9D9D9] text-white">
                 {/*The top part of the PopUp*/}
-                <div className="bg-white h-5"></div>
+                <div className="bg-[#D9D9D9] h-5"></div>
                 {/*The main part of the PopUp*/}
-                <div className="h-full m-[5%]">
-                    <h1>{prop.title}</h1>
+                <div className="h-full m-[5%] bg-black/80">
+                    <h1 className="mb-[5%]">{prop.title}</h1>
                     <p>{prop.text}</p>
                 </div>
             </div>
@@ -33,9 +33,9 @@ export default function PopUp(prop: popUpProps) { {/*title is reqiured ... or is
 
     if ("imgSrc" in prop) { {/*imgSrc is reqiured*/}
         return(
-            <div style={{ width: `${prop.width}px`, height: `${prop.height}px` }} className="border-white">
+            <div style={{ width: `${prop.width}px`, height: `${prop.height}px` }} className="border-solid border-[#D9D9D9] text-white">
                 {/*The top part of the PopUp*/}
-                <div className="bg-white h-10"></div>
+                <div className="bg-[#D9D9D9] h-10"></div>
                 {/*The main part of the PopUp*/}
                 <div><Image src={prop.imgSrc ?? ""} alt={prop.imgAlt} width={prop.width} height={prop.height-10} /></div>
             </div>
--- app/(frontend)/random1/page.tsx
+++ app/(frontend)/random1/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
   const example_text= "Ut sollicitudin justo vel congue sollicitudin. Phasellus a posuere magna, vitae malesuada augue. Maecenas dignissim, ipsum a maximus posuere, velit enim placerat est, in tempor nisl metus eget mi. Etiam interdum, nulla in dapibus euismod, augue elit dignissim velit, in pulvinar risus dolor ac tortor.Curabitur eu molestie nisl. Pellentesque commodo scelerisque magna ut malesuada. Quisque aliquam eget arcu sit amet malesuada. In in egestas turpis. Quisque consequat vel nulla at condimentum. Nulla facilisi. Proin gravida vestibulum libero. Mauris sagittis dignissim dignissim. Mauris cursus, diam eget mollis maximus, augue libero pharetra metus, a tincidunt leo velit eget risus. "
   return (
     <div className="container mx-auto my-6 px-4 space-y-4">
-      <PopUp width={300} height={200} title="Hello World" text={example_text} />
+      <PopUp width={300} height={1000} title="Hello World" text={example_text} />
     </div>
   );
 }
