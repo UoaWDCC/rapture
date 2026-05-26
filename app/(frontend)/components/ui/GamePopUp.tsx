0c5dcb57a5bed1fd3a176fb5facf9c473955b678
|rene <tendean.ireneandyna@gmail.com>
2026-05-26 22:28:52 +1200

    Adjusted Styling
    Adjusted to allow less restricted styling options. Tested with image and text components.

--- app/(frontend)/components/ui/GamePopUp.tsx
+++ app/(frontend)/components/ui/GamePopUp.tsx
@@ -1,29 +1,29 @@
 import Image from "next/image"
 
 type textPopUp = {
-    width: number
-    height: number
     title?: string
-    text?: string 
+    text?: string
+    className?: string
 }
 
 type imgPopUp = {
+    src: string
+    alt: string
     width: number
     height: number
-    imgSrc: string
-    imgAlt: string
+    className?: string
 }
 
 type popUpProps = textPopUp | imgPopUp
 
-export default function PopUp(prop: popUpProps) { {/*title is reqiured ... or is it?*/}
+export default function PopUp(prop: popUpProps,) { {/*title is reqiured ... or is it?*/}
     if ("title" in prop) {
         return(
-            <div style={{ width: `${prop.width}px`, height: `${prop.height}px` }} className="inline-block m-[2.5%] border-l border-r border-b border-dotted border-[#D9D9D9] text-white">
+            <div className={`inline-block border-l border-r border-b border-dotted border-[#D9D9D9] text-white ${prop.className}`}>
                 {/*The top part of the PopUp*/}
                 <div className="bg-[#D9D9D9] h-5"></div>
                 {/*The main part of the PopUp*/}
-                <div className="h-full m-[5%] bg-black/80">
+                <div className="h-full p-[4%] bg-black/80">
                     <h1 className="mb-[5%]">{prop.title}</h1>
                     <p>{prop.text}</p>
                 </div>
@@ -31,13 +31,13 @@ export default function PopUp(prop: popUpProps) { {/*title is reqiured ... or is
         );
     }
 
-    if ("imgSrc" in prop) { {/*imgSrc is reqiured*/}
+    if ("src" in prop) { {/*imgSrc is reqiured*/}
         return(
-            <div style={{ width: `${prop.width}px`, height: `${prop.height}px` }} className="border-solid border-[#D9D9D9] text-white">
+            <div className={`border-solid border-1 border-[#D9D9D9] text-white ${prop.className}`}>
                 {/*The top part of the PopUp*/}
-                <div className="bg-[#D9D9D9] h-10"></div>
+                <div className="bg-[#D9D9D9] h-5"></div>
                 {/*The main part of the PopUp*/}
-                <div><Image src={prop.imgSrc ?? ""} alt={prop.imgAlt} width={prop.width} height={prop.height-10} /></div>
+                <div><Image src={prop.src ?? ""} alt={prop.alt} width={prop.width} height={prop.height} sizes="100vw" /></div>
             </div>
         )
     }
