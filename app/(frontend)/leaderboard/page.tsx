68fc4fc3d181a15e7f90be41df360fab985f1eb7
Aeriel Cruz <aerielmatthew@gmail.com>
2026-06-01 17:18:00 +1200

    Feature/leaderboard page turner component design 69 (#74)
    * Feature/leaderboard page turner component design 69
    
    * chore: update comment
    
    * chore: remove redundant className

--- app/(frontend)/leaderboard/page.tsx
+++ app/(frontend)/leaderboard/page.tsx
@@ -1,29 +1,49 @@
 import { getPayload } from 'payload'
 import config from '@payload-config'
-import PlayerRowDisplay from './playerRowDisplay' 
+import PlayerRowDisplay from './playerRowDisplay'
 import Link from 'next/link'
+import { Pagination } from '@/app/(frontend)/components/ui/Pagination';
 import LeaderBoardSearch from './leaderboardSearch';
+
 export const dynamic = 'force-dynamic';
 
-export default async function LeaderboardPage() { //api part
+interface PageProps {
+  searchParams: Promise<{ page?: string; limit?: string }>;
+}
+
+export default async function LeaderboardPage({ searchParams }: PageProps) { //api part
+  const { page: pageParam, limit: limitParam } = await searchParams;
+
+  const page = Number(pageParam ?? 1);
+  const limit = Math.min(Number(limitParam ?? 10), 100);
+
   const payload = await getPayload({ config })
   const topPlayers = await payload.find({
-    collection: 'Players', 
-    sort: '-score',        
-    limit: 10,             
+    collection: 'Players',
+    sort: '-score',
+    limit,
+    page,
   })
 
   return ( //no styles.css file, can delete and change this whenever, just a placeholder
     <div className="p-8 font-sans flex flex-col items-center">
+
+      <Pagination
+        page={page}
+        totalPages={topPlayers.totalPages}
+        hasNextPage={topPlayers.hasNextPage}
+        hasPrevPage={topPlayers.hasPrevPage}
+      />
+
       <h1 className="text-3xl font-bold mb-6">Top 10 Leaderboard</h1>
       <Link href="/" className="mb-4 text-blue-500 hover:underline">Go back to Main Page</Link>
 
       <div className="flex flex-row w-full max-w-2xl gap-6">
         {topPlayers.docs.map((player, index) => (
           <PlayerRowDisplay //separate element used for ui design
-            key={player.id} 
-            player={player} 
-            rank={index + 1} 
+            key={player.id}
+            player={player}
+            rank={index + 1}
           />
         ))}
 
