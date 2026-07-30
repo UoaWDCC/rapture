9b3df88b6506406f1e4d9b840c5262f59de59e4d
lezha0 <leonazha0@icloud.com>
2026-07-30 22:22:09 +1200

    fix: contact page background colour (not #000000)

--- app/(frontend)/contact/page.tsx
+++ app/(frontend)/contact/page.tsx
@@ -4,10 +4,7 @@ import ContactForm from '../components/ContactForm';
 export default function ContactPage() {
   return (
     <div className="min-h-screen bg-[#070d0d] flex items-center justify-center p-8">
-      {/* 
-        This is a temporary container to help visualize the ContactForm. 
-        In the future, this will be wrapped by the coloured file tab component.
-      */}
+      {/* This is a temporary container to help visualize the ContactForm. */}
       <ContactForm />
     </div>
   );
