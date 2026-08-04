e32fa7a5c939e50d4bb014b1be8d1e49a67bec8d
zebony919 <zebony9@gmail.com>
2026-08-04 18:36:40 +1200

    feat: Can expand files

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -6,10 +6,114 @@ export default async function aboutPage() {
         <div className="mt-40 mb-80 flex flex-row">
             <ContactForm />
 
-            <div className="absolute left-0 h-full flex flex-row">
-                <ContactFile title="About Us (1)" bgColor="bg-red-800" />
-                <ContactFile title="About Us (2)" bgColor="bg-yellow-700" />
-                <ContactFile title="About Us (3)" bgColor="bg-green-800" />
+            <div className="absolute left-50 h-245 flex flex-row">
+                <ContactFile title="About Us (1)" bgColor="bg-red-800" sideHeight="100px" zIndex={3}>
+                    <div className="w-150 flex flex-col items-start">
+                        <h1 className="mb-20">About Us</h1>
+
+                        <p className="w-[70%] mb-10 text-left">Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                        </p>
+
+                        <p className="w-[70%] mb-10 text-left">Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                        </p>
+
+                        <p className="w-[70%] mb-10 text-left">Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                        </p>
+                    </div>
+                </ContactFile>
+
+                <ContactFile title="About Us (2)" bgColor="bg-green-700" sideHeight="200px" zIndex={2} mlValue={-550}>
+                    <div className="w-150 flex flex-col items-start">
+                        <h1 className="mb-20">About Us</h1>
+
+                        <p className="w-[70%] mb-10 text-left">Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                        </p>
+
+                        <p className="w-[70%] mb-10 text-left">Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                        </p>
+
+                        <p className="w-[70%] mb-10 text-left">Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                        </p>
+                    </div>
+                </ContactFile>
+
+                <ContactFile title="About Us (3)" bgColor="bg-yellow-800" sideHeight="300px" zIndex={1} mlValue={-550}>
+                    <div className="w-150 flex flex-col items-start">
+                        <h1 className="mb-20">About Us</h1>
+
+                        <p className="w-[70%] mb-10 text-left">Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                        </p>
+
+                        <p className="w-[70%] mb-10 text-left">Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                        </p>
+
+                        <p className="w-[70%] mb-10 text-left">Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                           Loren als ser pal Loren als ser pal Loren als ser pal
+                        </p>
+                    </div>
+                </ContactFile>
             </div>
         </div>
     )
