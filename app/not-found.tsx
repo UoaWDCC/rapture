d498adc1155ceeec45e1131b9dfd3f44c0a9c0db
|rene <tendean.ireneandyna@gmail.com>
2026-08-06 00:07:27 +1200

    Slight Styling Changes
    6th of August (picture sizing - it was too small).

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 export default function Custom404() {
   return (
     <div className="flex flex-col w-full h-screen mx-auto my-auto bg-black text-white items-center justify-center">
-      <div className="max-w-[30%] max-h-[20%] mt-[-2.5%]">
+      <div className="max-w-[80%] md:max-w-[25%] md:max-h-[20%] mt-[-2.5%]">
         <Image src="/Rapture_Large_2500-1000.png" alt="Logo.png" height={1000} width={1000} />
       </div>
-      <div className="w-[40%] mt-[-2%]">
+      <div className="w-[40%] mt-[-5%] md:mt-[-2%]">
         <p className="w-full text-center md:text-base text-xs">error #404</p>
         {/* <p className="w-full text-center">You have reached a dead end.</p> */}
-        <p className="w-full mb-[1.75%] text-center md:text-base text-xs"><a href="/" className="text-blue-500 text-shadow-blue-300 hover:text-shadow-sm hover:underline">Click here to return to the home page.</a></p>
+        <p className="w-full mb-[1.75%] text-center md:text-base text-xs"><a href="/" className="text-blue-500 text-shadow-blue-300 hover:text-shadow-sm hover:underline">click here to return to the home page.</a></p>
       </div>
     </div>
   )
