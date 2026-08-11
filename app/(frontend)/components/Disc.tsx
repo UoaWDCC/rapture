284f7cc61a8d90e4558a53b9c304c113241e5e8f
jAnderson2362 <jamesa2362@gmail.com>
2026-08-11 20:43:40 +1200

    feat: add curved RAPTURE ring text to disc component

--- app/(frontend)/components/Disc.tsx
+++ app/(frontend)/components/Disc.tsx
@@ -11,17 +11,32 @@ interface DiscProps {
     iconTop?: string
     iconRight?: string
     color: string
+    ringText?: string
 }
 
-export default function Disc({ name, url, image, icon, iconWidth, iconHeight, iconRight, iconTop, color }: DiscProps) {
+export default function Disc({ name, url, image, icon, iconWidth, iconHeight, iconRight, iconTop, color, ringText = "RAPTURE" }: DiscProps) {
     return (
         <Link href={url} target="_blank">
             <div className={`relative w-40 h-45 ${color} bg-opacity-10 rounded-xl overflow-hidden`}>
                 <Image src={image} width={102} height={134} alt={name} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full -mr-2" />
-                {icon && <Image src={icon} alt={name + " icon"} width={iconWidth || 56} height={iconHeight || 56} 
-                className= {`absolute ${iconTop || 'top-4'} ${iconRight || 'right-2'} object-contain`} />}
-                <p className="absolute bottom-4 left-1/2 -translate-x-1/2 text-white text-[24px] uppercase" 
-                style={{ fontFamily: "'Nova Cut', cursive" }}>{name}</p>
+                <svg viewBox="0 0 100 100" className="absolute top-1/2 left-1/2 w-20 h-20 pointer-events-none -translate-x-1/2 -translate-y-1/2" style={{ marginLeft: "-4px", marginTop: "5.5px" }}>
+                    <defs>
+                        <path id={`ring-${name}`} d="M 50,50 m -28,0 a 28,28 0 1,1 56,0 a 28,28 0 1,1 -56,0" fill="none" />
+                    </defs>
+                    <text fill="#ffffff" fontSize="6" letterSpacing="1" style={{ fontFamily: "'Nova Cut', cursive" }}>
+                        <textPath href={`#ring-${name}`} startOffset="0%">{ringText}</textPath>
+                    </text>
+                    <text fill="#ffffff" fontSize="6" letterSpacing="1" style={{ fontFamily: "'Nova Cut', cursive" }}>
+                        <textPath href={`#ring-${name}`} startOffset="33.33%">{ringText}</textPath>
+                    </text>
+                    <text fill="#ffffff" fontSize="6" letterSpacing="1" style={{ fontFamily: "'Nova Cut', cursive" }}>
+                        <textPath href={`#ring-${name}`} startOffset="66.66%">{ringText}</textPath>
+                    </text>
+                </svg>
+                {icon && <Image src={icon} alt={name + " icon"} width={iconWidth || 56} height={iconHeight || 56}
+                    className={`absolute ${iconTop || 'top-4'} ${iconRight || 'right-2'} object-contain`} />}
+                <p className="absolute bottom-4 left-1/2 -translate-x-1/2 text-white text-[24px] uppercase"
+                    style={{ fontFamily: "'Nova Cut', cursive" }}>{name}</p>
             </div>
         </Link>
     )
