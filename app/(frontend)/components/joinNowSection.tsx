8d4e657992278e3fc4ed5f15ad6e489770ea08a8
Aeriel Cruz <aerielmatthew@gmail.com>
2026-08-18 18:38:16 +1200

    feat: add newTab prop

--- app/(frontend)/components/joinNowSection.tsx
+++ app/(frontend)/components/joinNowSection.tsx
@@ -5,12 +5,16 @@ import GlitchReveal from "./GlitchReveal";
 const JoinButton = ({
   href,
   children,
+  newTab,
 }: {
   href: string;
   children: string;
+  newTab?: boolean;
 }) => (
   <Link
     href={href}
+    target={newTab ? "_blank" : undefined}
+    rel={newTab ? "noopener noreferrer" : undefined}
     className="bg-brand-green/30 flex-1 flex items-center justify-center py-4 px-6 text-brand-white -tracking-[0.1em] text-sm md:text-base hover:bg-brand-green/10 transition-colors"
   >
     {children}
@@ -38,7 +42,7 @@ export function JoinNowSection() {
               style={{ boxShadow: "0 0 12px 2px rgba(32, 128, 90, 0.6)" }}
             >
               <div className=" border border-brand-green flex flex-col divide-y divide-brand-green md:min-w-90">
-                <JoinButton href="https://discord.gg">DISCORD</JoinButton>
+                <JoinButton href="https://discord.gg" newTab>DISCORD</JoinButton>
                 <div className="h-3"></div>
                 <JoinButton href="/">WEBSITE</JoinButton>
               </div>
