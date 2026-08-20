1c6d67ffec7062fde83189f6de60bcc1fe2f88b2
Dahyun Ha <162376263+dha524@users.noreply.github.com>
2026-08-20 12:15:50 +1200

    fix: edited disc component sizing

--- app/(frontend)/components/Disc.tsx
+++ app/(frontend)/components/Disc.tsx
@@ -21,14 +21,14 @@ export default function Disc({ name, url, image, icon, iconWidth, iconHeight, ic
         <Link href={url} target="_blank">
             <div className={`group relative w-60 h-65 ${color} bg-opacity-10 rounded-xl overflow-hidden`}>
                 <Image src={image} width={122} height={154} alt={name} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full -mr-2" />
-                <svg viewBox="0 0 100 100" className="absolute top-1/2 left-1/2 w-20 h-20 pointer-events-none -translate-x-1/2 -translate-y-1/2" style={{ marginLeft: "-4px", marginTop: "5.5px" }}>
+                <svg viewBox="0 0 100 100" className="absolute top-1/2 left-1/2 z-10 w-20 h-20 overflow-visible pointer-events-none -translate-x-1/2 -translate-y-1/2" style={{ marginLeft: "-4px", marginTop: "5.5px" }}>
                     <defs>
-                        <path id={`ring-${name}`} d="M 48,54 m -28,0 a 28,28 0 1,1 56,0 a 28,28 0 1,1 -56,0" fill="none" />
+                        <path id={`ring-${name}`} d="M 48,54 m -42,0 a 42,42 0 1,1 84,0 a 42,42 0 1,1 -84,0" fill="none" />
                     </defs>
-                    <circle cx="48" cy="56" r="36" fill={discColor} fillOpacity="0.35" />
-                    <circle cx="48" cy="56" r="14" fill="#000000" fillOpacity="0.5" />
-                    <circle cx="48" cy="56" r="14" fill="none" stroke={ringColor} strokeWidth="1" />
-                    <circle cx="48" cy="56" r="36" fill="none" stroke={ringColor} strokeWidth="1" />
+                    <circle cx="48" cy="54" r="56" fill={discColor} fillOpacity="0.35" />
+                    <circle cx="48" cy="54" r="14" fill="#000000" fillOpacity="0.5" />
+                    <circle cx="48" cy="54" r="14" fill="none" stroke={ringColor} strokeWidth="1" />
+                    <circle cx="48" cy="54" r="56" fill="none" stroke={ringColor} strokeWidth="1" />
                     <g className="disc-ring">
                         <text fill="#ffffff" fontSize="6" letterSpacing="1" style={{ fontFamily: "'Fira Mono'" }}>
                             <textPath href={`#ring-${name}`} startOffset="0%">{ringText}</textPath>
