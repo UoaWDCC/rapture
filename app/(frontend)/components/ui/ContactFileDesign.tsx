e005e1c7dce45bb73180dcb650174350df59779d
|rene <tendean.ireneandyna@gmail.com>
2026-08-27 15:27:12 +1200

    Final Styling Changes
    Tried making it responsive, didn't work very well.

--- app/(frontend)/components/ui/ContactFileDesign.tsx
+++ app/(frontend)/components/ui/ContactFileDesign.tsx
@@ -19,7 +19,7 @@ export default function ContactFile(prop: contactFileProps) {
     const [open, setOpen] = useState(false);
 
     return(
-        <div className={`flex flex-row bg-transparent transition-transform duration-300 ${open ? prop.translateValueClosed ?? 'translate-x-[-200px]' : prop.translateValueOpen ?? '-translate-x-[100%]'} ${prop.className}`}
+        <div className={`flex flex-row bg-transparent transition-transform duration-300 ${open ? prop.translateValueClosed ?? '-translate-x-50' : prop.translateValueOpen ?? '-translate-x-full'} ${prop.className}`}
                 style={{ zIndex: open ? prop.zIndex : prop.zIndex ?? 20, 
                          marginLeft: prop.mlValue ?? 0,
                 }}>
@@ -31,9 +31,9 @@ export default function ContactFile(prop: contactFileProps) {
             <div className={`flex flex-row relative -ml-0.5 h-fit writing-mode-vertical bg-transparent hover:cursor-pointer`}
                     style={{ marginTop: prop.sideHeight ?? '7px' }}
                     onClick={() => setOpen(!open)}>
-                <div className={`h-4 w-10 skew-y-20 ${prop.bgColor} ${prop.classNameSide}`}></div>
-                <p className={`-mt-1.75 -mb-11.75 py-ppx px-2.25 ${prop.bgColor} text-2xl`}>{prop.title}</p>
-                <div className={`h-4 w-10 -skew-y-20 mt-10 ${prop.bgColor}`}></div>
+                <div className={`h-4 w-5.125 md:w-10 skew-y-20 ${prop.bgColor} ${prop.classNameSide}`}></div>
+                <p className={`-mt-1.75 -mb-11.75 w-5 md:w-10 py-ppx px-2.25 ${prop.bgColor} text-sm md:text-xl `}>{prop.title}</p>
+                <div className={`h-4 w-5 md:w-10 -skew-y-20 mt-10 ${prop.bgColor}`}></div>
             </div>
         </div>
     );
