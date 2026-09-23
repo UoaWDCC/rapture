b1d4fbfbf8bf72a14520d715e59aa0564a789d4f
Shazeel Ali <75409190+ShazeelAli@users.noreply.github.com>
2026-09-23 22:04:17 +1200

    fix:minor change with apos

--- lib/email/email_templates/resetPassword.tsx
+++ lib/email/email_templates/resetPassword.tsx
@@ -1,15 +1,22 @@
 import { Body, Html, Heading, Text } from "@react-email/components";
 
-export default function ResetPasswordEmail({ name, url }: { name: string, url: string }) {
+export default function ResetPasswordEmail({
+  name,
+  url,
+}: {
+  name: string;
+  url: string;
+}) {
   return (
     <Html>
       <Body>
         <Heading>Password Reset Request</Heading>
         <Text>Hi, {name},</Text>
         <Text>
-          Here is the <a href={url}>link</a> to direct you to the reset password page.
+          Here is the <a href={url}>link</a> to direct you to the reset password
+          page.
         </Text>
-        <Text>If the request wasn't from you, just ignore.</Text>
+        <Text>If the request wasn\&apos;t from you, just ignore.</Text>
         <Text>Thank you.</Text>
         <Text>
           just some random text just some random text just some random text just
